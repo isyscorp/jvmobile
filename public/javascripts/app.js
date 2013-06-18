5b62a7888185ef5eea89ae3ffb422e14ca2c1217
Jinggo Villamor <jingcleovil@gmail.com>
2013-06-18 18:04:35 +0800

    update menus

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,27 @@
 function hideSplash() {
-  $.mobile.changePage("#home", "fade");
+  //$.mobile.changePage("#home", "fade");
 }
 
-
 $(document).bind("pageinit", function(){
-	hideSplash();
+	//hideSplash();
+	/*
+	$('#menupanel').on('click', 'a', function(e){
+		e.preventDefault();
+		var page = $(this).prop('href');
+
+		$.mobile.changePage("#wall", "slide");
+		
+		$(this).addClass('active');
+		$(this).parent().siblings().find('a').removeClass('active');
+		console.log('triggered');
+	})
+*/
 });
+
+$("#home").on("pageinit",function(e){
+	console.log('home init');
+});
+
+$("#wall").on("pageinit",function(e){
+	console.log('wall init');
+})
